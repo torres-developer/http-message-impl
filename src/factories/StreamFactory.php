8ea8519999ef89d03e9012d2c424ecaee1673dd2
torres-developer <torres.dev@disroot.org>
2023-01-12 23:44:58 +0000

    Start implementing PSR 17

---
+++ src/factories/StreamFactory.php
@@ -0,0 +1,54 @@
+<?php
+
+/**
+ *        http-message-impl - An implementation of the psr-7
+ *        Copyright (C) 2023  João Torres
+ *
+ *        This program is free software: you can redistribute it and/or modify
+ *        it under the terms of the GNU Affero General Public License as
+ *        published by the Free Software Foundation, either version 3 of the
+ *        License, or (at your option) any later version.
+ *
+ *        This program is distributed in the hope that it will be useful,
+ *        but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *        MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *        GNU Affero General Public License for more details.
+ *
+ *        You should have received a copy of the GNU Affero General Public License
+ *        along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ * @package TorresDeveloper\\HTTPMessage\\Factories
+ * @author João Torres <torres.dev@disroot.org>
+ * @copyright Copyright (C) 2023  João Torres
+ * @license https://www.gnu.org/licenses/agpl-3.0.txt GNU Affero General Public License
+ * @license https://opensource.org/licenses/AGPL-3.0 GNU Affero General Public License version 3
+ *
+ * @since 1.0.0
+ * @version 1.1.0
+ */
+
+namespace TorresDeveloper\HTTPMessage\Factories;
+
+use Psr\Http\Message\StreamFactoryInterface;
+use Psr\Http\Message\StreamInterface;
+use TorresDeveloper\HTTPMessage\Stream;
+
+class StreamFactory implements StreamFactoryInterface
+{
+    public function createStream(string $content = ''): StreamInterface
+    {
+        return new Stream($content);
+    }
+
+    public function createStreamFromFile(
+        string $filename,
+        string $mode = 'r'
+    ): StreamInterface {
+        return new Stream(new \SplFileObject($filename, $mode));
+    }
+
+    public function createStreamFromResource($resource): StreamInterface
+    {
+        throw new \RuntimeException("resource != \SplFileObject");
+    }
+}
---
+++ src/factories/UploadedFileFactory.php
@@ -0,0 +1,60 @@
+<?php
+
+/**
+ *        http-message-impl - An implementation of the psr-7
+ *        Copyright (C) 2023  João Torres
+ *
+ *        This program is free software: you can redistribute it and/or modify
+ *        it under the terms of the GNU Affero General Public License as
+ *        published by the Free Software Foundation, either version 3 of the
+ *        License, or (at your option) any later version.
+ *
+ *        This program is distributed in the hope that it will be useful,
+ *        but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *        MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *        GNU Affero General Public License for more details.
+ *
+ *        You should have received a copy of the GNU Affero General Public License
+ *        along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ * @package TorresDeveloper\\HTTPMessage\\Factories
+ * @author João Torres <torres.dev@disroot.org>
+ * @copyright Copyright (C) 2023  João Torres
+ * @license https://www.gnu.org/licenses/agpl-3.0.txt GNU Affero General Public License
+ * @license https://opensource.org/licenses/AGPL-3.0 GNU Affero General Public License version 3
+ *
+ * @since 1.0.0
+ * @version 1.1.0
+ */
+
+namespace TorresDeveloper\HTTPMessage\Factories;
+
+use Psr\Http\Message\StreamInterface;
+use Psr\Http\Message\UploadedFileFactoryInterface;
+use Psr\Http\Message\UploadedFileInterface;
+use TorresDeveloper\HTTPMessage\UploadedFile;
+
+class UploadedFileFactory implements UploadedFileFactoryInterface
+{
+    public function createUploadedFile(
+        StreamInterface $stream,
+        ?int $size = null,
+        int $error = \UPLOAD_ERR_OK,
+        ?string $clientFilename = null,
+        ?string $clientMediaType = null
+    ): UploadedFileInterface {
+        if (!$stream->isReadable()) {
+            throw new \InvalidArgumentException(
+                "Stream needs to be readable."
+            );
+        }
+
+        return new UploadedFile(
+            $stream,
+            $size,
+            $error,
+            $clientFilename,
+            $clientMediaType
+        );
+    }
+}
